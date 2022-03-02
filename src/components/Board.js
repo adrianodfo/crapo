db785ad6fa6ea1e2f6bef091756deb2293538483
Adriano Di Foggi Oliveira <adrianodfo@outlook.com>
2022-03-02 22:49:43 +0000

    Refactor board.

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,49 +6,22 @@ export default function Board(props) {
   const cardDropped = (droppedCard, dropTarget) => {
     props.onCardPlaced(droppedCard, dropTarget);
   }
-  return (
-    //TODO: is it possible to eliminate Board and BoardRow?
-    <div className="Board">
-      <div className="BoardRow">
-        <div class="BoardColumn Left">
-          <WorkingPile Cards={props.WorkingPiles[0]} OriginIndex="0" DropTarget="0" onCardPlaced={cardDropped} />
-          <Stack />
-        </div>
-        <div class="BoardColumn Right">
-          <WorkingPile Cards={props.WorkingPiles[1]} OriginIndex="1" DropTarget="1" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
+
+  const board = props.WorkingPiles.map((element, index) => {
+    return (
+      <div>
+        <WorkingPile 
+          Cards={element}
+          OriginIndex={index}
+          DropTarget={index}
+          onCardPlaced={cardDropped}
+          Key={index} />
+        <Stack />
       </div>
-      <div className="BoardRow">
-        <div class="BoardColumn Left">
-          <WorkingPile Cards={props.WorkingPiles[2]} OriginIndex="2" DropTarget="2" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-        <div class="BoardColumn Right">
-          <WorkingPile Cards={props.WorkingPiles[3]} OriginIndex="3" DropTarget="3" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-      </div>
-      <div className="BoardRow">
-        <div class="BoardColumn Left">
-          <WorkingPile Cards={props.WorkingPiles[4]} OriginIndex="4" DropTarget="4" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-        <div class="BoardColumn Right">
-          <WorkingPile Cards={props.WorkingPiles[5]} OriginIndex="5" DropTarget="5" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-      </div>
-      <div className="BoardRow">
-        <div class="BoardColumn Left">
-          <WorkingPile Cards={props.WorkingPiles[6]} OriginIndex="6" DropTarget="6" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-        <div class="BoardColumn Right">
-          <WorkingPile Cards={props.WorkingPiles[7]} OriginIndex="7" DropTarget="7" onCardPlaced={cardDropped}/>
-          <Stack />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  });
+
+  return <div className="Board">
+    {board}
+  </div>
 }
